90504748b49a16731b65200f1fae4e19a26c6e0d
汤宏伟 <907575428@qq.com>
2017-12-15 16:26:24 +0800

    完成邀请函

--- yaoqinghan/js/script.js
+++ yaoqinghan/js/script.js
@@ -11,6 +11,15 @@ var swiper = new Swiper('.swiper-container', {
     // 前进按钮
     navigation: {
         nextEl: '.swiper-button-next'
+    },
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
     }
 });
 var mp3=document.querySelector("#mp3");
